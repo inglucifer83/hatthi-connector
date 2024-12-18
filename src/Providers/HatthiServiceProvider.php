ed22db3e419e4e55daa2a583df9bfc47a68b90f4
inglucifer83 <stanescu83@gmail.com>
2024-12-18 21:29:01 +0200

    Update HatthiServiceProvider.php

--- src/Providers/HatthiServiceProvider.php
+++ src/Providers/HatthiServiceProvider.php
@@ -3,23 +3,16 @@
 namespace Hatthi\Connector\Providers;
 
 use Hatthi\Connector\Console\Commands\Connect;
+use Hatthi\Connector\Console\Commands\Listen;
 use Illuminate\Support\ServiceProvider;
 
 class HatthiServiceProvider extends ServiceProvider {
 
-    public function register() {
-
-    }
-
-    /**
-     * Bootstrap services.
-     *
-     * @return void
-     */
     public function boot(): void {
         if ($this->app->runningInConsole()) {
             $this->commands([
                 Connect::class,
+                Listen::class,
             ]);
         }
     }
